4649379d5f57eb2d6f460caf59604dddd471044b
Xinyi Zhang <celine.zhang@kellogg.northwestern.edu>
2024-04-19 00:43:20 +0000

    Practice airbnb

--- practice-4.sql
+++ practice-4.sql
@@ -1,5 +1,11 @@
 -- How many listings are in Lincoln Park?
 
+SELECT COUNT(neighborhood)
+FROM listings
+WHERE neighborhood = "Lincoln Park"
+GROUP BY neighborhood;
+
+
 -- +----------+
 -- | 272      |
 -- +----------+
